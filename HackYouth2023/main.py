20550a442c14e9194c147fa05979f283f394aff9
Le Dinh Nguyen <ledinhnguyen00@gmail.com>
2023-03-06 21:55:23 +0700

    update bank.css, dashboard.html

--- HackYouth2023/main.py
+++ HackYouth2023/main.py
@@ -24,9 +24,12 @@ def login():
 
 
 @app.route("/signup")
-def signuo():
+def signup():
     return render_template("signup.html")
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
